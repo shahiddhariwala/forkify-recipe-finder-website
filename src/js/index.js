09b643b7bbf6798463e3a5049063a06066d6b0f1
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-05-19 21:21:17 +0530

    Implemented Search Result Pagination

--- src/js/index.js
+++ src/js/index.js
@@ -41,3 +41,14 @@ elements.searchForm.addEventListener('submit',event=>
     controlSearch();
 });
 
+elements.searchResult.addEventListener('click',event=>
+{
+    const btn = event.target.closest('.btn-inline');
+    if(btn)
+    {
+        const goToPage = parseInt(btn.dataset.goto,10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result,goToPage);
+    }
+});
+
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,28 +1,25 @@
-import { elements } from "./base";
+import {
+    elements
+} from "./base";
 
-export const clearInput = () =>
-{   
-    elements.searchInput.value='';
+export const clearInput = () => {
+    elements.searchInput.value = "";
 };
 
-export const clearResults = () =>
-{   
-    elements.searchResultList.innerHTML='';
+export const clearResults = () => {
+    elements.searchResultList.innerHTML = "";
+    elements.searchResultPages.innerHTML = "";
 };
 
-const limitRecipeTitle = (title,limit =17) =>
-{
+const limitRecipeTitle = (title, limit = 17) => {
     const newTitle = [];
-    if(title.length > limit)
-    {
-        title.split(' ').reduce((acc,cur)=>
-        {
-            if(acc+cur.length <= limit)
-            {
+    if (title.length > limit) {
+        title.split(" ").reduce((acc, cur) => {
+            if (acc + cur.length <= limit) {
                 newTitle.push(cur);
             }
-            return acc+cur.length;
-        },0);
+            return acc + cur.length;
+        }, 0);
 
         //return short title
         return `${newTitle.join(" ")} ...`;
@@ -33,19 +30,53 @@ const limitRecipeTitle = (title,limit =17) =>
 
 export const getInput = () => elements.searchInput.value;
 const renderRecipe = (recipe) => {
-  const markup = ` <li>
+    const markup = ` <li>
     <a class="results__link" href="#${recipe.recipe_id}}">
         <figure class="results__fig">
             <img src="${recipe.image_url}" alt="${recipe.title}">
         </figure>
         <div class="results__data">
-            <h4 class="results__name">${limitRecipeTitle(recipe.title,20)}</h4>
+            <h4 class="results__name">${limitRecipeTitle(recipe.title, 20)}</h4>
             <p class="results__author">${recipe.publisher}</p>
         </div>
     </a>
 </li>`;
-  elements.searchResultList.insertAdjacentHTML('beforeend',markup);
+    elements.searchResultList.insertAdjacentHTML("beforeend", markup);
 };
-export const renderResults = (recipes) => {
-  recipes.forEach(renderRecipe);
+const createButton = (page, type) => `
+
+                <button class="btn-inline results__btn--${type}" data-goto=${type==='prev'? page-1:page+1}>
+                    <svg class="search__icon">
+                        <use href="img/icons.svg#icon-triangle-${type==='prev'? 'left':'right'}"></use>
+                    </svg>
+                    <span>Page ${type==='prev'? page-1:page+1}</span>
+                </button>
+`;
+
+const renderButtons = (page, numResults, pagePerResult) => {
+    const pages = Math.ceil(numResults / pagePerResult);
+    let button;
+    if (page === 1 && pages > 1) {
+        // Only Button to go to next page
+        button = createButton(page,'next');
+    } else if (page === pages && pages > 1) {
+        //Only Button to go prev page
+        button = createButton(page,'prev');
+    } else if (page < pages) {
+        //Go in both direction
+        button = `${createButton(page,'prev')} ${createButton(page,'next')}`;
+       
+    }
+    elements.searchResultPages.insertAdjacentHTML('beforeend',button);
+
+};
+export const renderResults = (recipes, page = 1, pagePerResult = 10) => {
+
+    // render results of current page
+    const start = (page - 1) * pagePerResult;
+    const end = page * pagePerResult;
+    recipes.slice(start, end).forEach(renderRecipe);
+
+    //render pagination
+    renderButtons(page,recipes.length,pagePerResult);
 };
