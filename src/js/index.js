62fd716e57b133936f26692d82f2fe2f3d39c754
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-05-19 12:39:00 +0530

    fetch only works on  modern browsers but for older browsers its not supported so we use axious we can install it by 'npm install axios --save'

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,18 @@
 // Global app controller
-
-import x from './test';
-
-console.log(`I Imported ${x} from another module !!!`);
+import axios from 'axios';
+/*
+fetch only works on  modern browsers but for older browsers its not supported
+so we use axious
+we can install it by 'npm install axios --save'
+*/
+async function getResult(query) {
+    //one step we get directly json data not raw
+    try {
+        const result = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
+        const recipes = result.data.recipes;
+        console.log(recipes);
+    } catch (error) {
+        console.log(error)
+    }
+}
+getResult('pizza');
